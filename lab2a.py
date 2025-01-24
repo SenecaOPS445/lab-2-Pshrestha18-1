5bf17ed49fc249f1fbc9cfbd0d451f34373c3fb2
Pranav Shrestha <pshrestha18@myseneca.ca>
2025-01-24 13:36:39 -0500

    lab2a completed

--- lab2a.py
+++ lab2a.py
@@ -4,10 +4,10 @@
 # Date Created: 2025/01/24
 
 # Script to demonstrate the use of input() function and simple logic
-# Prompt the user to enter their name and greet them
 
-# Asking the user for their name
-user_name = input("Please enter your name: ")
+# Define variables
+name = "Jon"
+age = 20
 
-# Displaying a greeting message
-print(f"Hello, {user_name}! Welcome to Lab 2.")
+# Print the output
+print(f"Hi {name}, you are {age} years old.")
