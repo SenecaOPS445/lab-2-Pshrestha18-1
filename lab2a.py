15374ceeac3ac49d9ba42270fd3fe58da0b844fc
Pranav Shrestha <pshrestha18@myseneca.ca>
2025-01-24 13:19:17 -0500

    lab2a completed

---
+++ lab2a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Pranav Shrestha
+# Author ID: 113964225
+# Date Created: 2025/01/24
+
+# Script to demonstrate the use of input() function and simple logic
+# Prompt the user to enter their name and greet them
+
+# Asking the user for their name
+user_name = input("Please enter your name: ")
+
+# Displaying a greeting message
+print(f"Hello, {user_name}! Welcome to Lab 2.")
